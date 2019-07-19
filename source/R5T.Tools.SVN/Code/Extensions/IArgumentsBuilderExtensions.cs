c44ca41ac2d31b5bf2d403fe405d0dce210d70ae
MinexAutomation <minex.automation@gmail.com>
2019-07-19 12:29:16 -0400

    Added extensions to the Neapolis command-line arguments builder interface for SVN status arguments.

---
+++ source/R5T.Tools.SVN/Code/Extensions/IArgumentsBuilderExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using R5T.Neapolis;
+
+
+namespace R5T.Tools.SVN
+{
+    public static class IArgumentsBuilderExtensions
+    {
+        public static IArgumentsBuilder AddVerbose(this IArgumentsBuilder argumentsBuilder)
+        {
+            argumentsBuilder.AddFlagShort("v");
+
+            return argumentsBuilder;
+        }
+
+        public static IArgumentsBuilder SetDepth(this IArgumentsBuilder argumentsBuilder, string depth)
+        {
+            argumentsBuilder
+                .AddNameValue(nameValue =>
+                {
+                    nameValue
+                        .SetNameFull("depth")
+                        .SetValue(depth)
+                        ;
+                });
+
+            return argumentsBuilder;
+        }
+
+        public static IArgumentsBuilder ForInstanceOnly(this IArgumentsBuilder argumentsBuilder)
+        {
+            argumentsBuilder.SetDepth("empty");
+
+            return argumentsBuilder;
+        }
+
+        public static IArgumentsBuilder AddXml(this IArgumentsBuilder argumentsBuilder)
+        {
+            argumentsBuilder.AddFlagFull("xml");
+
+            return argumentsBuilder;
+        }
+    }
+}
