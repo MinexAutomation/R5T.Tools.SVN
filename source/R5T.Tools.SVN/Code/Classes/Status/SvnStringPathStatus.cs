588feb73e66f99b479c1ecdabc8fc59a99cbc35a
MinexAutomation <minex.automation@gmail.com>
2019-07-19 12:18:45 -0400

    Added a string-valued path SVN status type.

---
+++ source/R5T.Tools.SVN/Code/Classes/Status/SvnStringPathStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Tools.SVN
+{
+    public class SvnStringPathStatus
+    {
+        public string Path { get; set; }
+        public ItemStatus ItemStatus { get; set; }
+    }
+}
