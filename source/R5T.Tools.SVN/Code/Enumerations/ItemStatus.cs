aff9b2ef2316afa1206bcf0efb7c54ee2189f1ed
MinexAutomation <minex.automation@gmail.com>
2019-07-19 12:32:35 -0400

    Added SVN items status values for paths that are outside the working copy, or not-found because they are within an ignored or unversioned directory.

--- source/R5T.Tools.SVN/Code/Enumerations/ItemStatus.cs
+++ source/R5T.Tools.SVN/Code/Enumerations/ItemStatus.cs
@@ -8,7 +8,18 @@ namespace R5T.Tools.SVN
     /// </summary>
     public enum ItemStatus
     {
+        /// <summary>
+        /// Does not exist.
+        /// </summary>
         None = 0,
+        /// <summary>
+        /// An item that is outside of a working copy directory hierarchy.
+        /// </summary>
+        NotWorkingCopy,
+        /// <summary>
+        /// Indicates an item is either in an ignored or unversioned directory.
+        /// </summary>
+        NotFound,
 
         NoModifications, // a.k.a. "normal"
 
@@ -23,6 +34,9 @@ namespace R5T.Tools.SVN
         Modified,
         Obstructed,
         Replaced,
+        /// <summary>
+        /// Not under source control.
+        /// </summary>
         Unversioned,
     }
 }
