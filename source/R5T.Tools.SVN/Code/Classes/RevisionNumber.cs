df06325b8bca495100ee72131e74453ed5cf5667
MinexAutomation <minex.automation@gmail.com>
2019-09-22 23:26:43 -0700

    Added a RevisionNumber class.

---
+++ source/R5T.Tools.SVN/Code/Classes/RevisionNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace R5T.Tools.SVN
+{
+    public class RevisionNumber
+    {
+        public const int Invalid = -1;
+
+
+        #region Static
+
+        public static bool IsInvalid(int revisionNumber)
+        {
+            var output = revisionNumber == RevisionNumber.Invalid;
+            return output;
+        }
+
+        #endregion
+    }
+}
